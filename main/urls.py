40aadd6962ae8ce66b8a2a334654d134e016b866
John Kaita <johnkaitadocuments@gmail.com>
2022-02-26 06:57:50 +0300

    Initital Commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
+
 from main import views as mainviews
 
 urlpatterns = [
     path('', mainviews.homepage, name="homepage"),
+    path('newspage', mainviews.newspage, name="newspage"),
+    path('cartoonspage', mainviews.cartoonspage, name="cartoonspage"),
+    path('jabspage', mainviews.jabspage, name="jabspage"),
+    path('teampage', mainviews.teampage, name="teampage"),
+    path('quotespage', mainviews.quotespage, name="quotespage"),
+    path('socialproblemspage', mainviews.socialproblemspage, name="socialproblemspage"),
+    path('proverbspage', mainviews.proverbspage, name="proverbspage"),
+    path('longcomicspage', mainviews.longcomicspage, name="longcomicspage"),
+    path('imageviewer/<path:theimage>', mainviews.imageviewer, name="imageviewer"),
+    path('illustrationnamespage', mainviews.illustrationnamespage, name="illustrationnamespage"),
+    path('illustrationcontentpage/<str:illustrationname>', mainviews.illustrationcontentpage,
+         name="illustrationcontentpage"),
+    path('pdfviewerpage/<path:thepdf>', mainviews.pdfviewerpage,
+         name="pdfviewerpage"),
+    path('conthumournamespage', mainviews.conthumournamespage, name="conthumournamespage"),
+    path('conthumourcontentpage/<str:conthumourname>', mainviews.conthumourcontentpage, name="conthumourcontentpage"),
+    path('newsviewer/<str:newsid>', mainviews.newsviewer,  name="newsviewer"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,111 @@
-from datetime import datetime
 from django.shortcuts import render
-from django.template.context_processors import request
 
-# from testPython import db, summationEggsCollected_DateQueryRange, allsummary, summationExpenses_DateSummary, \
-#     summationSales_DateQuery, allclients, totalEggsCollectedThisMonth, totalSalesForThisMonth, \
-#     totalExpensesForThisMonth, alleggs, allsales, allexpenses, allCustomerNames, historyForSpecificClient, \
-#     summationSales, summationEggsCollected, summationExpenses, doesPassExist, allexpenses_farm, summationExpenses_Farm, \
-#     summationExpenses_DateSummary_Farm, allsales_farm,allCustomerNames_farms, historyForSpecificClientfarm
+from testPython import loadhumour, loadjabs, loadquotes, loadsocialproblems, loadproverbs, loadlongcomics, \
+    loadillustrationnames, loadillustrationcontent, loadconthumournames, loadconthumourcontent, loadvideos, \
+    loadmagazines, loadnewscategories, dbs, loadnewslocations
 
-def  homepage(request):
-    response = render(request, "index.html", {"summary": ""})
+monthsummaryDict = {}
+monthsummaryDict['loadhumour'] = loadhumour()
+monthsummaryDict['loadjabs'] = loadjabs()
+monthsummaryDict['loadquotes'] = loadquotes()
+monthsummaryDict['loadsocialproblems'] = loadsocialproblems()
+monthsummaryDict['loadproverbs'] = loadproverbs()
+monthsummaryDict['longcomics'] = loadlongcomics()
+monthsummaryDict['illustrationnames'] = loadillustrationnames()
+monthsummaryDict['conthumournames'] = loadconthumournames()
+monthsummaryDict['loadvideos'] = loadvideos()
+monthsummaryDict['loadmagazines'] = loadmagazines()
+monthsummaryDict['loadnewscategories'] = loadnewscategories()
+monthsummaryDict['loadnewslocations'] = loadnewslocations()
+
+def homepage(request):
+    response = render(request, "index.html", {"summary": monthsummaryDict})
+    return response
+
+
+def cartoonspage(request):
+    response = render(request, "cartoonspage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def imageviewer(request, theimage):
+    response = render(request, "imageviewer.html", {"theimage": theimage})
+    return response
+
+
+def jabspage(request):
+    response = render(request, "jabspage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def quotespage(request):
+    response = render(request, "quotespage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def socialproblemspage(request):
+    response = render(request, "socialproblemspage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def proverbspage(request):
+    response = render(request, "proverbspage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def longcomicspage(request):
+    response = render(request, "longcomicspage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def illustrationnamespage(request):
+    response = render(request, "illustrationnamespage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def illustrationcontentpage(request, illustrationname):
+    monthsummaryDict['illustrationcontent'] = loadillustrationcontent(illustrationname)
+    response = render(request, "illustrationcontentpage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def conthumournamespage(request):
+    response = render(request, "conthumournamespage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def conthumourcontentpage(request, conthumourname):
+    monthsummaryDict['conthumourcontent'] = loadconthumourcontent(conthumourname)
+    response = render(request, "conthumourcontentpage.html", {"summary": monthsummaryDict})
+    return response
+
+
+def pdfviewerpage(request, thepdf):
+    monthsummaryDict['thepdf'] = thepdf
+    response = render(request, "pdfviewer.html", {"summary": monthsummaryDict})
+    return response
+
+def newspage(request):
+    response = render(request, "newspage.html", {"summary": monthsummaryDict})
+    return response
+
+def newsviewer(request, newsid):
+    identity = int(newsid) * -1
+
+    newsdict = {}
+    newsdict['category'] = dbs.reference(f'news/news/{identity}/category').get()
+    newsdict['description'] = dbs.reference(f'news/news/{identity}/description').get()
+    newsdict['date'] = dbs.reference(f'news/news/{identity}/fulldate').get()
+    newsdict['image'] = dbs.reference(f'news/news/{identity}/image').get()
+    newsdict['location'] = dbs.reference(f'news/news/{identity}/location').get()
+    newsdict['name'] = dbs.reference(f'news/news/{identity}/name').get()
+    newsdict['timestamp'] = dbs.reference(f'news/news/{identity}/timestamp').get()
+    newsdict['name'] = dbs.reference(f'news/news/{identity}/name').get()
+
+    response = render(request, "newsviewer.html", {"summary": newsdict})
+    return response
+
+
+def teampage(request):
+    response = render(request, "team.html", {"summary": monthsummaryDict})
     return response
