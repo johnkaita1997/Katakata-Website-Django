2f4a0b86e73db6a4c159c0a50589a1006fb3d2cd
John Kaita <johnkaitadocuments@gmail.com>
2022-02-26 07:55:49 +0300

    Added middleware

--- katakatawebsite/settings.py
+++ katakatawebsite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'katakatawebsite.urls'
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+
